e034545078f23df6b2a73c401f0afbd1ff73fb9e
lohith967 <72453060+lohith967@users.noreply.github.com>
2021-07-01 19:45:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,57 @@ camera=document.getElementById("camera");
 
 Webcam.attach("#camera");
 
-function takesnapshot(){
+function take_snapshot(){
     Webcam.snap(function(data_uri){
         document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
     });
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1="first pridiction is"+prediction1;
+    speak_data_2="And second prediction is"+prediction2;
+var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+synth.speak(utterThis);
+}
+function check(){
+    img = document.getElementById('capture image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error,results){
+if (error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction1 = results[0].label;
+    prediction2 = results[1].label;
+    speak();
+    if(results[0].label=="happy"){
+        document.getElementById("update_emoji").innerHTML="&#128522;"
+    }
+    if(results[0].label=="sad"){
+        document.getElementById("update_emoji").innerHTML="&#128532;"
+    }
+    if(results[0].label=="angry"){
+        document.getElementById("update_emoji").innerHTML="&#128545;"
+    }
+    if(results[1].label=="happy"){
+        document.getElementById("update_emoji").innerHTML="&#128522;"
+    }
+    if(results[1].label=="sad"){
+        document.getElementById("update_emoji").innerHTML="&#128532;"
+    }
+    if(results[1].label=="angry"){
+        document.getElementById("update_emoji").innerHTML="&#128545;"
+    }
+}
 }
